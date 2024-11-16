122fc00821b72e6d469749116c8b181412cba311
raden wahid <61126583+radenwahid@users.noreply.github.com>
2024-11-16 23:00:16 +0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,7 +1,5 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-import time
-import httpx
 
 template = (
     "You are tasked with extracting specific information from the following text content: {dom_content}. "
@@ -21,19 +19,9 @@ def parse_with_ollama(dom_chunks, parse_description):
     parsed_results = []
     
     for i, chunk in enumerate(dom_chunks, start=1):
-        try:
-            start_time = time.time()  # Start measuring time
-            response = chain.invoke({"dom_content": chunk, "parse_description": parse_description})
-            elapsed_time = time.time() - start_time  # Measure elapsed time
-            print(f"Parsed batch {i} of {len(dom_chunks)} in {elapsed_time:.2f} seconds")
-            parsed_results.append(response)
-            
-        except httpx.RequestError as e:
-            print(f"Request error while processing batch {i}: {e}")
-            parsed_results.append("")
-
-        except Exception as e:
-            print(f"Unexpected error while processing batch {i}: {e}")
-            parsed_results.append("")
+        response = chain.invoke({"dom_content": chunk, "parse_description": parse_description})
+        print(f"Parsed batch {i} of {len(dom_chunks)} ")
+        parsed_results.append(response)
+        
         
     return "\n".join(parsed_results)
